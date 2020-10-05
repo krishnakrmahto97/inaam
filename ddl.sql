4240e8be1101f2821c05caf178962953279ebc76
Madhurjya Roy <roymadhurjya@gmail.com>
2020-10-05 15:58:57 +0530

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -7,76 +7,76 @@ create database inaam;
 
 create table admin
 (
-    id     uuid primary key,
-    name   text,
-    secret text
+    id     varchar(36) primary key,
+    name   varchar(255),
+    secret varchar(255)
 );
 
 create table realm
 (
-    id              uuid primary key,
-    name            text,
-    currency_symbol text
+    id              varchar(36) primary key,
+    name            varchar(255),
+    currency_symbol varchar(3)
 );
 
 create table realm_attribute
 (
-    realm_id uuid references realm (id),
-    name     text,
-    value    text,
+    realm_id varchar(36) references realm (id),
+    name     varchar(255),
+    value    varchar(255),
     primary key (realm_id, name)
 );
-create index on realm_attribute (realm_id);
+create index realm_attribute_realm_id_idx on realm_attribute (realm_id);
 
 
 create table client
 (
-    id       uuid primary key,
-    realm_id uuid references realm (id),
-    name     text,
-    secret   text,
+    id       varchar(36) primary key,
+    realm_id varchar(36) references realm (id),
+    name     varchar(255),
+    secret   varchar(255),
     unique (realm_id, name)
 );
-create index on client (realm_id);
-create index on client (name);
+create index client_realm_id_idx on client (realm_id);
+create index client_name_idx on client (name);
 
 create table user_details
 (
-    id       uuid primary key,
-    realm_id uuid references realm (id),
-    name     text
+    id       varchar(36) primary key,
+    realm_id varchar(36) references realm (id),
+    name     varchar(255)
 );
-create index on user_details (realm_id);
-create index on user_details (name);
+create index user_details_realm_id_idx on user_details (realm_id);
+create index user_details_name_idx on user_details (name);
 
 create table user_attribute
 (
-    user_id uuid references user_details (id),
-    name    text,
-    value   text,
+    user_id varchar(36) references user_details (id),
+    name    varchar(255),
+    value   varchar(255),
     primary key (user_id, name)
 );
 
 create table group_details
 (
-    id       uuid unique,
-    realm_id uuid references realm (id),
-    name     text not null,
+    id       varchar(36) unique,
+    realm_id varchar(36) references realm (id),
+    name     varchar(255) not null,
     primary key (id)
 );
 
 create table user_group
 (
-    user_id  uuid references user_details (id),
-    group_id uuid references group_details (id),
+    user_id  varchar(36) references user_details (id),
+    group_id varchar(36) references group_details (id),
     primary key (user_id, group_id)
 );
 
 create table coin
 (
-    id              uuid,
-    realm_id uuid references realm (id),
-    name            text not null,
+    id              varchar(36),
+    realm_id        varchar(36) references realm (id),
+    name            varchar(255) not null,
     conversion_rate numeric(10, 2),
     primary key (realm_id, id),
     unique (realm_id, name)
@@ -84,8 +84,8 @@ create table coin
 
 create table user_coin
 (
-    user_id uuid references user_details (id),
-    coin_id uuid references user_details (id),
+    user_id varchar(36) references user_details (id),
+    coin_id varchar(36) references user_details (id),
     balance numeric,
     primary key (user_id, coin_id)
 );
