d3a871844e2d5c7c5a39310ca17673900f98d208
Madhurjya Roy <roymadhurjya@gmail.com>
2020-10-08 10:23:53 +0530

    Increase AccessToken expiry time

--- src/main/java/io/inaam/main/service/security/SecurityConstants.java
+++ src/main/java/io/inaam/main/service/security/SecurityConstants.java
@@ -6,8 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityConstants {
     public static final String SECRET = "SECRET_KEY";
-    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 1800;
-    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 86_400;
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 86_400;
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 1_728_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
