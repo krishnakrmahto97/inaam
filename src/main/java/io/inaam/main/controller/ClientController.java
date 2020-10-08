e8370a6ff0674c55082a4e60a2810eaaa7cd3782
Dhawal negi <dhawal.negi1@gmail.com>
2020-10-08 21:14:27 +0530

    Swagger for client controller (#22)
    * swagger for client controller
    
    * ddl-auto change to validate

--- src/main/java/io/inaam/main/controller/ClientController.java
+++ src/main/java/io/inaam/main/controller/ClientController.java
@@ -3,6 +3,7 @@ package io.inaam.main.controller;
 import io.inaam.main.dto.ClientDto;
 import io.inaam.main.entity.Client;
 import io.inaam.main.service.ClientService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,26 +11,31 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/{realm}/client")
 public class ClientController {
 
     private final ClientService clientService;
 
-    @PostMapping("/{realm}/client")
+    @PostMapping
+    @ApiOperation(value = "Create a new client under the realm")
     public String createClient(@PathVariable String realm, @RequestBody ClientDto clientDto) {
         return clientService.createClient(realm, clientDto);
     }
 
-    @GetMapping("/{realm}/client")
+    @GetMapping
+    @ApiOperation(value = "Get list of all clients in a realm")
     public List<Client> getClients(@PathVariable String realm) {
         return clientService.getClients(realm);
     }
 
-    @GetMapping("/{realm}/client/{clientName}")
+    @GetMapping("/{clientName}")
+    @ApiOperation(value = "Get detail of a client in a realm")
     public Client getClient(@PathVariable String realm, @PathVariable String clientName) {
         return clientService.getClient(realm, clientName);
     }
 
-    @GetMapping("/{realm}/client/{clientName}/secret")
+    @GetMapping("/{clientName}/secret")
+    @ApiOperation(value = "Get secret for a client in a realm")
     public String getClientSecret(@PathVariable String realm, @PathVariable String clientName) {
         return clientService.getClientSecret(realm, clientName);
     }
