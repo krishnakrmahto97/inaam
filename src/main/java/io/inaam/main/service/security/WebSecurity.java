6164d162613571554055d02d95e013a50c167207
Madhurjya Roy <roymadhurjya@gmail.com>
2020-10-08 20:26:29 +0530

    Exclude swagger UI from auth

--- src/main/java/io/inaam/main/service/security/WebSecurity.java
+++ src/main/java/io/inaam/main/service/security/WebSecurity.java
@@ -23,6 +23,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().disable()
